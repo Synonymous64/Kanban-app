0a74970690f4d6a432a4587f7e39e551c204cde7
Synonymous64 <prajwalurkude007@gmail.com>
2023-05-17 20:14:57 +0530

    initial setup, header component created and redux setup

---
+++ kanban-task-management-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Center from "./components/Center";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      {/* Header Section */}
+      <Header />
+      {/* Center Section */}
+      <Center />
+    </div>
+  )
+}
+
+export default App;
